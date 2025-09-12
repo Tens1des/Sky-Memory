13601726980f347c44726f8efa1311d6738e304c
Tens1des <romankotov3213@gmail.com>
2025-09-12 21:28:50 +0300

    Initial Commit

---
+++ Sky Memory/Sky_MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sky_MemoryApp.swift
+//  Sky Memory
+//
+//  Created by Рома Котов on 12.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Sky_MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
